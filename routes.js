88b93a605735cb70f5341c628328f0d0fedbbf09
AlexPurcell <alextpurcell05@gmail.com>
2021-07-14 23:12:19 -0600

    New Method for filter check

--- routes.js
+++ routes.js
@@ -10,21 +10,52 @@ router.get('/', (req, res) => {
 
 let todos = [];
 
-let DoubleFilter = false
+
+
 
 router.post("/add_todo", (req, res) => {
 // Next Step: IF there is a TODO that is the same exact thing, I should check to see if it is equal to the same input then throw an error.
 
-const addTodo = (todos, newItem) => {
-  const isTodoPresent = todos.some((todos) => todos.title === newItem);
-  if(isTodoPresent) {
-    console.log("Error: TODO already exists")
-    return todos;
+
+// METHOD 1
+
+
+// const addTodo = (todos, newItem) => {
+//   const isTodoPresent = todos.some((todos) => todos.title === newItem);
+//   if(isTodoPresent) {
+//     console.log("Error: TODO already exists")
+//     return todos;
+//   }
+//   return todos.concat({title: newItem, completed: false})
+// }
+
+// todos = addTodo(todos, req.body.add_todo_input);
+
+
+
+// METHOD 2
+
+const postedTitle = req.body.add_todo_input;
+
+
+let alreadyExists = false;
+
+for (let i = 0; i < todos.length; i++) {
+  const item = todos[i];
+  if(item.title === postedTitle) {
+    alreadyExists = true;
+    break;
   }
-  return todos.concat({title: newItem, completed: false})
 }
 
-todos = addTodo(todos, req.body.add_todo_input);
+console.log('Already Exists?:', alreadyExists)
+
+if (alreadyExists === false) {
+  todos.push({title: postedTitle, complete: false})
+}
+else {
+  console.log("This TODO already exists!")
+}
 
     res.redirect("/")
   });
